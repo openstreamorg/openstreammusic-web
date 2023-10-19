63a70927d765f41b8da069bc952818644a65d723
techguy16 <88870951+techguy16@users.noreply.github.com>
2023-10-19 16:15:24 +1300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+
 module.exports = {
     entry: './renderer.js',
     output: {
@@ -10,15 +11,21 @@ module.exports = {
             'node_modules': path.join(__dirname, 'node_modules'),
         },
         fallback: {
-            "fs": false,
-            "tls": false,
-            "net": false,
-            "path": false,
-            "zlib": false,
-            "http": false,
-            "https": false,
-            "stream": false,
-            "crypto": false
+            "fs": require.resolve("fs"),
+            "tls": require.resolve("tls"),
+            "net": require.resolve("net"),
+            "path": require.resolve("path"),
+            "zlib": require.resolve("zlib"),
+            "http": require.resolve("http"),
+            "https": require.resolve("https"),
+            "stream": require.resolve("stream"),
+            "crypto": require.resolve("crypto"),
+            "util": require.resolve("util"),
+            "buffer": require.resolve("buffer"),
+            "querystring": require.resolve("querystring-es3"),
+            "timers": require.resolve("timers-browserify"),
+            "vm": require.resolve("vm-browserify")
         }
-    }
+    },
+    mode: 'development'
 };
