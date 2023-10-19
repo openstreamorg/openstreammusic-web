a70b70f0ef48e8e283b3b3769955617c570f176f
techguy16 <88870951+techguy16@users.noreply.github.com>
2023-10-19 15:45:38 +1300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,6 @@ module.exports = {
             url: require.resolve('url'),
             fs: require.resolve('fs'),
             assert: require.resolve('assert'),
-            https: require.resolve('https-browserify'),
             os: require.resolve('os-browserify/browser'),
             buffer: require.resolve('buffer'),
             stream: require.resolve('stream-browserify')
