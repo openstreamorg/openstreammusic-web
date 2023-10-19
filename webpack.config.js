3f4cbb8ce6c5f4835f9a52bfa75438093e23f52d
techguy16 <88870951+techguy16@users.noreply.github.com>
2023-10-19 15:07:07 +1300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,33 @@
 const path = require('path');
-    module.exports = {
-      entry: './renderer.js',
-      output: {
+module.exports = {
+    entry: './renderer.js',
+    output: {
         path: path.resolve(__dirname),
         filename: 'webpack.dist.js'
-      },
-      resolve: {
+    },
+    resolve: {
         alias: {
-          'node_modules': path.join(__dirname, 'node_modules'),
+            'node_modules': path.join(__dirname, 'node_modules'),
         }
-      }
-    };
+        fallback: {
+            // Use can only include required modules. Also install the package.
+            // for example: npm install --save-dev assert
+            url: require.resolve('url'),
+            fs: require.resolve('fs'),
+            assert: require.resolve('assert'),
+            crypto: require.resolve('crypto-browserify'),
+            http: require.resolve('stream-http'),
+            https: require.resolve('https-browserify'),
+            os: require.resolve('os-browserify/browser'),
+            buffer: require.resolve('buffer'),
+            stream: require.resolve('stream-browserify'),
+        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+            Buffer: ['buffer', 'Buffer'],
+        })
+    ]
+}
+};
